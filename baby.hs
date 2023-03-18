b3785b46505dfeb50d7c979996260faa8de1e88a
Amaechi Abuah <abuah1999@gmail.com>
2023-03-18 06:39:05 -0400

    first commit

---
+++ baby.hs
@@ -0,0 +1,10 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x < 100 then
+			doubleMe x
+			else x
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
